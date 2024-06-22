32a29f06b378c30e523f391fbdac1b9a10e06101
Viet Anh Nguyen <vietanh.dev@gmail.com>
2024-06-22 14:13:51 +0000

    Update deployment scripts

--- restart-server.sh
+++ restart-server.sh
@@ -1,2 +1,2 @@
-sudo docker compose -f tools/compose/traefik-secure.yml --env-file .env.production stop
-sudo docker compose -f tools/compose/traefik-secure.yml --env-file .env.production up -d
+docker compose -f compose.yml --env-file .env stop
+docker compose -f tools/compose/traefik-secure.yml --env-file .env up -d
---
+++ server-logs.sh
@@ -0,0 +1,1 @@
+docker compose -f compose.yml --env-file .env logs -n 1000 -f
