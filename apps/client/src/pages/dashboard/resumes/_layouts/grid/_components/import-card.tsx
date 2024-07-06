fef918447c257bac59222e1c10a674431e362ef3
Viet-Anh Nguyen <vietanh.dev@gmail.com>
2024-07-06 16:49:35 +0700

    Add padding

--- apps/client/src/pages/dashboard/resumes/_layouts/grid/_components/import-card.tsx
+++ apps/client/src/pages/dashboard/resumes/_layouts/grid/_components/import-card.tsx
@@ -13,7 +13,7 @@ export const ImportResumeCard = () => {
       }}
       className="bg-blue-100 text-blue-500 border-2 border-dashed border-blue-500 flex items-center justify-center h-full"
     >
-      <div className="flex flex-col items-center justify-center text-center">
+      <div className="flex flex-col items-center justify-center text-center p-2">
         <img src="/icons/upload.svg" alt="Create a new resume" className="w-12 h-12 mx-auto" />
         <h4 className="text-gray-600 font-bold mt-8">{t`Import an existing resume`}</h4>
         <p className="text-xs opacity-75 text-gray-600 mt-2">{t`LinkedIn, JSON Resume, etc.`}</p>
